ffc791e8046ebcec848b7bf5566859bdeb38bf3e
Amine El Baghdadi <75886237+NoobJsPerson@users.noreply.github.com>
2021-05-09 18:46:29 +0000

    update index.js

---
+++ index.js
@@ -0,0 +1,54 @@
+const fs = require('fs');
+module.exports = class EditClient {
+  constructor(defaultFile,relative){
+    this.defaultFile = relative?__dirname+defaultFile:defaultFile;
+  }
+  async editProperty(property,value,file = this.defaultFile,relative){
+    if(!file) throw Error("No JSON to edit");
+    if(relative && (this.defaultFile != file || !file.startsWith(__dirname))) file = __dirname+file;
+    const content = await fs.promises.readFile(file);
+    const fileObject = JSON.parse(content);
+    fileObject[property] = value;
+    
+    return await fs.promises.writeFile(file, JSON.stringify(fileObject));
+  }
+  async deleteProperty(property,file = this.defaultFile,relative){
+    if(!file) throw Error("No JSON to edit");
+    if(relative && (this.defaultFile != file || !file.startsWith(__dirname))) file = __dirname+file;
+    const content = await fs.promises.readFile(file);
+    const fileObject = JSON.parse(content);
+    delete fileObject[property];
+    
+    return await fs.promises.writeFile(file, JSON.stringify(fileObject));
+  }
+  async deleteProperties(properties,file = this.defaultFile,relative){
+    if(!file) throw Error("No JSON to edit")
+    if(relative && (this.defaultFile != file || !file.startsWith(__dirname))) file = __dirname+file;
+    if(!(properties instanceof Array)) throw Error("properties must be an Array");
+    const content = await fs.promises.readFile(file);
+    const fileObject = JSON.parse(content);
+    for(let i = 0;i < properties.length;i++){
+    delete fileObject[properties[i]];
+    }
+    return await fs.promises.writeFile(file, JSON.stringify(fileObject));
+  }
+  async editProperties(properties,values ,file = this.defaultFile,relative){
+    if(!file) throw Error("No JSON to edit");
+    if(relative && (this.defaultFile != file || !file.startsWith(__dirname))) file = __dirname+file;
+    if(!(properties instanceof Array)) throw Error("properties must be an Array");
+    if(!(values instanceof Array)) throw Error("values must be an Array");
+    const content = await fs.promises.readFile(file);
+    const fileObject = JSON.parse(content);
+    for(let i = 0;i < properties.length;i++){
+    fileObject[properties[i]] = values[i];
+    }
+    
+    return await fs.promises.writeFile(file, JSON.stringify(fileObject));
+  }
+  async getValue(property,file = this.defaultFile,relative){
+    if(!file) throw Error("No JSON to get value from");
+    if(relative && (this.defaultFile != file || !file.startsWith(__dirname))) file = __dirname+file;
+    const content = await fs.promises.readFile(file);
+    return content[property];
+  }
+}
