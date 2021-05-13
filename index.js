5a1a75659314315d139533030fafce76eb7ffdb6
Amine El Baghdadi <75886237+NoobJsPerson@users.noreply.github.com>
2021-05-13 15:15:09 +0000

    update index.js

--- index.js
+++ index.js
@@ -18,39 +18,39 @@ module.exports = class EditClient {
     this.dirname = dirname
     if (defaultFile) this.defaultFile = applyrelative(defaultFile, dirname, relative);
   }
-  async write(obj = {}, sync, file = this.defaultFile, relative) {
+  write(obj = {}, file = this.defaultFile, relative) {
     if (!file) throw ReferenceError("No JSON to edit");
     if (!(obj instanceof Object)) throw Error("properties must be an Object");
     file = applyrelative(file, this.dirname, relative)
-    if (sync) return fs.writeFileSync(file, JSON.stringify(obj));
-    else return await fs.promises.writeFile(file, JSON.stringify(obj));
+    fs.writeFile(file, obj, () => {});
+    return obj;
   }
-  async set(property, value, sync, file = this.defaultFile, relative) {
+  async set(property, value, file = this.defaultFile, relative) {
     if (!file) throw ReferenceError("No JSON to edit");
     file = applyrelative(file, this.dirname, relative)
     const content = await fs.promises.readFile(file);
     const fileObject = JSON.parse(content);
     fileObject[property] = value;
-    return await this.write(fileObject, sync, file);
+    return this.write(fileObject, file);
   }
-  async delete(property, sync, file = this.defaultFile, relative) {
+  async delete(property, file = this.defaultFile, relative) {
     if (!file) throw ReferenceError("No JSON to edit");
     file = applyrelative(file, this.dirname, relative);
     const content = await fs.promises.readFile(file);
     const fileObject = JSON.parse(content);
     delete fileObject[property];
-    return await this.write(fileObject, sync, file);
+    return this.write(fileObject, file);
   }
-  async bulkDelete(properties, sync, file = this.defaultFile, relative) {
+  async bulkDelete(properties, file = this.defaultFile, relative) {
     if (!file) throw ReferenceError("No JSON to edit")
     file = applyrelative(file, this.dirname, relative);
     if (!(properties instanceof Array)) throw TypeError("properties must be an Array");
     const content = await fs.promises.readFile(file);
     const fileObject = JSON.parse(content);
     for (let i = 0; i < properties.length; i++) delete fileObject[properties[i]];
-    return await this.write(fileObject, sync, file);
+    return this.write(fileObject, file);
   }
-  async bulkSet(properties, values, sync, file = this.defaultFile, relative) {
+  async bulkSet(properties, values, file = this.defaultFile, relative) {
     if (!file) throw ReferenceError("No JSON to edit");
     file = applyrelative(file, this.dirname, relative);
     if (!(properties instanceof Array)) throw TypeError("properties must be an Array");
@@ -58,57 +58,143 @@ module.exports = class EditClient {
     const content = await fs.promises.readFile(file);
     const fileObject = JSON.parse(content);
     for (let i = 0; i < properties.length; i++) fileObject[properties[i]] = values[i];
-    return await this.write(fileObject, sync, file);
+    return this.write(fileObject, file);
   }
-  async get(property, sync, file = this.defaultFile, relative) {
+  async get(property, file = this.defaultFile, relative) {
     if (!file) throw ReferenceError("No JSON to get value from");
     file = applyrelative(file, this.dirname, relative);
-    const content = sync ? fs.readFileSync(file) : await fs.promises.readFile(file);
+    const content = await fs.promises.readFile(file);
     return property ? content[property] : content;
   }
-  async push({ file = this.defaultFile, sync, relative } = {}, property, ...values) {
+  async push({ file = this.defaultFile, relative } = {}, property, ...values) {
     if (!file) throw ReferenceError("No JSON to edit");
     file = applyrelative(file, this.dirname, relative);
     const content = await fs.promises.readFile(file);
     const fileObject = JSON.parse(content);
     if (!(property != "this" ? fileObject[property] : fileObject instanceof Array)) throw TypeError("property must have a value of Array");
     (property != "this" ? fileObject[property] : fileObject).push(...values)
-    return await this.write(fileObject, sync, file);
+    return this.write(fileObject, file);
   }
-  async unshift({ file = this.defaultFile, sync, relative } = {}, property, ...values) {
+  async unshift({ file = this.defaultFile, relative } = {}, property, ...values) {
     if (!file) throw ReferenceError("No JSON to edit");
     file = applyrelative(file, this.dirname, relative);
     const content = await fs.promises.readFile(file);
     const fileObject = JSON.parse(content);
     if (!(property != "this" ? fileObject[property] : fileObject instanceof Array)) throw TypeError("property must have a value of Array");
     (property != "this" ? fileObject[property] : fileObject).unshift(...values)
-    return await this.write(fileObject, sync, file);
+    return this.write(fileObject, file);
   }
-  async pop(property, sync, file = this.defaultFile, relative) {
+  async pop(property, file = this.defaultFile, relative) {
     if (!file) throw ReferenceError("No JSON to edit");
     file = applyrelative(file, this.dirname, relative);
     const content = await fs.promises.readFile(file);
     const fileObject = JSON.parse(content);
     if (!(property != "this" ? fileObject[property] : fileObject instanceof Array)) throw TypeError("property must have a value of Array");
     (property != "this" ? fileObject[property] : fileObject).pop()
-    return await this.write(fileObject, sync, file);
+    return this.write(fileObject, file);
   }
-  async shift(property, sync, file = this.defaultFile, relative) {
+  async shift(property, file = this.defaultFile, relative) {
     if (!file) throw ReferenceError("No JSON to edit");
     file = applyrelative(file, this.dirname, relative);
     const content = await fs.promises.readFile(file);
     const fileObject = JSON.parse(content);
     if (!(property != "this" ? fileObject[property] : fileObject instanceof Array)) throw TypeError("property must have a value of Array");
     (property != "this" ? fileObject[property] : fileObject).shift()
-    return await this.write(fileObject, sync, file);
+    return this.write(fileObject, file);
   }
-  async splice({ file = this.defaultFile, relative, sync } = {}, property, start, deleteCount, ...values) {
+  async splice({ file = this.defaultFile, relative, } = {}, property, start, deleteCount, ...values) {
     if (!file) throw ReferenceError("No JSON to edit");
     file = applyrelative(file, this.dirname, relative);
     const content = await fs.promises.readFile(file);
     const fileObject = JSON.parse(content);
     if (!(property != "this" ? fileObject[property] : fileObject instanceof Array)) throw TypeError("property must have a value of Array");
     (property != "this" ? fileObject[property] : fileObject).splice(start, deleteCount, ...values);
-    return await this.write(fileObject, sync, file);
+    return this.write(fileObject, file);
+  }
+  setSync(property, value, file = this.defaultFile, relative) {
+    if (!file) throw ReferenceError("No JSON to edit");
+    file = applyrelative(file, this.dirname, relative)
+    const content = fs.readFileSync(file);
+    const fileObject = JSON.parse(content);
+    fileObject[property] = value;
+    return this.write(fileObject, file);
+  }
+  deleteSync(property, file = this.defaultFile, relative) {
+    if (!file) throw ReferenceError("No JSON to edit");
+    file = applyrelative(file, this.dirname, relative);
+    const content = fs.readFileSync(file);
+    const fileObject = JSON.parse(content);
+    delete fileObject[property];
+    return this.write(fileObject, file);
+  }
+  bulkDeleteSync(properties, file = this.defaultFile, relative) {
+    if (!file) throw ReferenceError("No JSON to edit")
+    file = applyrelative(file, this.dirname, relative);
+    if (!(properties instanceof Array)) throw TypeError("properties must be an Array");
+    const content = fs.readFileSync(file);
+    const fileObject = JSON.parse(content);
+    for (let i = 0; i < properties.length; i++) delete fileObject[properties[i]];
+    return this.write(fileObject, file);
+  }
+  bulkSetSync(properties, values, file = this.defaultFile, relative) {
+    if (!file) throw ReferenceError("No JSON to edit");
+    file = applyrelative(file, this.dirname, relative);
+    if (!(properties instanceof Array)) throw TypeError("properties must be an Array");
+    if (!(values instanceof Array)) throw TypeError("values must be an Array");
+    const content = fs.readFileSync(file);
+    const fileObject = JSON.parse(content);
+    for (let i = 0; i < properties.length; i++) fileObject[properties[i]] = values[i];
+    return this.write(fileObject, file);
+  }
+  getSync(property, file = this.defaultFile, relative) {
+    if (!file) throw ReferenceError("No JSON to get value from");
+    file = applyrelative(file, this.dirname, relative);
+    const content = fs.readFileSync(file);
+    return property ? content[property] : content;
+  }
+  pushSync({ file = this.defaultFile, relative } = {}, property, ...values) {
+    if (!file) throw ReferenceError("No JSON to edit");
+    file = applyrelative(file, this.dirname, relative);
+    const content = fs.readFileSync(file);
+    const fileObject = JSON.parse(content);
+    if (!(property != "this" ? fileObject[property] : fileObject instanceof Array)) throw TypeError("property must have a value of Array");
+    (property != "this" ? fileObject[property] : fileObject).push(...values)
+    return this.write(fileObject, file);
+  }
+  unshiftSync({ file = this.defaultFile, relative } = {}, property, ...values) {
+    if (!file) throw ReferenceError("No JSON to edit");
+    file = applyrelative(file, this.dirname, relative);
+    const content = fs.readFileSync(file);
+    const fileObject = JSON.parse(content);
+    if (!(property != "this" ? fileObject[property] : fileObject instanceof Array)) throw TypeError("property must have a value of Array");
+    (property != "this" ? fileObject[property] : fileObject).unshift(...values)
+    return this.write(fileObject, file);
+  }
+  popSync(property, file = this.defaultFile, relative) {
+    if (!file) throw ReferenceError("No JSON to edit");
+    file = applyrelative(file, this.dirname, relative);
+    const content = fs.readFileSync(file);
+    const fileObject = JSON.parse(content);
+    if (!(property != "this" ? fileObject[property] : fileObject instanceof Array)) throw TypeError("property must have a value of Array");
+    (property != "this" ? fileObject[property] : fileObject).pop()
+    return this.write(fileObject, file);
+  }
+  shiftSync(property, file = this.defaultFile, relative) {
+    if (!file) throw ReferenceError("No JSON to edit");
+    file = applyrelative(file, this.dirname, relative);
+    const content = fs.readFileSync(file);
+    const fileObject = JSON.parse(content);
+    if (!(property != "this" ? fileObject[property] : fileObject instanceof Array)) throw TypeError("property must have a value of Array");
+    (property != "this" ? fileObject[property] : fileObject).shift()
+    return this.write(fileObject, file);
+  }
+  spliceSync({ file = this.defaultFile, relative, } = {}, property, start, deleteCount, ...values) {
+    if (!file) throw ReferenceError("No JSON to edit");
+    file = applyrelative(file, this.dirname, relative);
+    const content = fs.readFileSync(file);
+    const fileObject = JSON.parse(content);
+    if (!(property != "this" ? fileObject[property] : fileObject instanceof Array)) throw TypeError("property must have a value of Array");
+    (property != "this" ? fileObject[property] : fileObject).splice(start, deleteCount, ...values);
+    return this.write(fileObject, file);
   }
 };
