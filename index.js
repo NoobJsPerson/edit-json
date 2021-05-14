8019f741cf43f309ec85b5d3e393dfc884cb2f37
Amine El Baghdadi <75886237+NoobJsPerson@users.noreply.github.com>
2021-05-14 16:40:21 +0000

    update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 
-function applyrelative(file, dirname, relative) {
-  return dirname && relative && !file.startsWith(dirname) ? dirname + (file.startsWith(".") ? file.replace(".", "") : !file.startsWith("/") ? "/" + file : file) : file.startsWith(".") ? file : "."+file;
+function applyrelative(file, dirname, relative){
+  if (!file.startsWith("./") || !file.startsWith("/")) file = "/" + file;
+  return dirname && relative && !file.startsWith(dirname) ? dirname + (file.startsWith(".") ? file.replace(".", "") : file) : !file.startsWith(".") ? "." + file : file;
 }
 module.exports = class EditClient {
   /**
